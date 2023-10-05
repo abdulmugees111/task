c7718648e90a2ccdd4feb3713b1814fdc52a71de
abdulmugees111 <abdulmugees44@gmail.com>
2023-10-05 23:00:24 +0500

    add footer height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import "../../style.css"
+ 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-black flex justify-center items-center py-2">
+    <footer className="bg-black flex justify-center items-center py-2 footer">
       <p className='text-white'>&copy; 2023 Your Company. All rights reserved.</p>
     </footer>
   );
